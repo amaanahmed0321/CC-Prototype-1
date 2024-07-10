ba84138537a70d6ee6996e943a8010c9b85a4b8f
Humnit <kzheng5011@gmail.com>
2024-07-10 11:47:01 -0500

    Testing out some concepts.

---
+++ CC Prototype 1/CC Prototype 1/new.swift
@@ -0,0 +1,18 @@
+//
+//  new.swift
+//  CC Prototype 1
+//
+//  Created by sermacbook_16 on 7/10/24.
+//
+
+import SwiftUI
+
+struct new: View {
+    var body: some View {
+        Image("new_yellow")
+    }
+}
+
+#Preview {
+    new()
+}
