94cac4cef10f0ba4466d7d83414d56e5c65885f0
Esther V <ejvalentin12@gmail.com>
2024-07-15 09:21:49 -0500

    profile

--- CC Prototype 1/CC Prototype 1/ProfileTab.swift
+++ CC Prototype 1/CC Prototype 1/ProfileTab.swift
@@ -7,21 +7,59 @@
 
 import SwiftUI
 
-struct ProfileTab: View {
-    let background = Color(red: 153/255, green: 195/255, blue: 247/255)
-    
-    var body: some View {
-        VStack {
-            Text("Profile")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .bold()
-                .foregroundStyle(.black)
-            background
-           
-            Spacer()
+        struct ProfileTab: View {
+            let background = Color(red: 153/255, green: 195/255, blue: 247/255)
             
-        }
+            var body: some View {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Welcome Esther!")
+                            .font(.headline)
+                        Text("Your profile")
+                            .font(.subheadline)
+                            .bold()
+                    }
+                    Spacer()
+                }
+                .padding()
+                .scenePadding(.top)
         
+                HStack{
+                    Circle()
+                        .frame(width: 130, height: 130)
+                        .foregroundColor(.pink)
+                        .position(x: 50, y: 60)
+                        .padding()
+                    VStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .frame(width: 200, height: 150)
+                            .foregroundColor(.yellow)
+                        Text("Your Milestones")
+                            .bold()
+                            .position(x:80, y:0)
+                            .padding()
+                    }
+                    
+                }
+                .padding()
+                ZStack{
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .frame(width: 320, height: 150)
+                }
+        
+            
+
+           
+                    NavigationStack{
+                        List{
+                            NavigationLink("Settings"){}
+                            NavigationLink("Accessibility"){}
+                            NavigationLink("Privacy & Safety"){}
+                            NavigationLink("Account"){}
+                        }
+                        
+                    }
+                
 
     }
 }
@@ -29,3 +67,4 @@ struct ProfileTab: View {
 #Preview {
     ProfileTab()
 }
+
